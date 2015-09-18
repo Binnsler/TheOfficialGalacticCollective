dda5fc80ad6055a599ddbd29895eb824b78d9177
Jamie Binns <jamie.m.binns@gmail.com>
2015-09-18 14:01:18 -0600

    See what happens

--- controllers/index.js
+++ controllers/index.js
@@ -101,7 +101,7 @@ var indexController = {
 
 		// CHECK IF THE IP USER ALREADY LIKED THIS POST
 		Post.find({_id: req.body._id}, function(err, userData){
-			
+			console.log(userData);
 			if(
 				userData.likedByIp.indexOf(req.body.userIP) === -1
 				){
