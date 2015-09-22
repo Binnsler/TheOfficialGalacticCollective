4543addc5c30ad8f129d7b935b2f60e111ba3b63
Jamie Binns <jamie.m.binns@gmail.com>
2015-09-22 09:39:32 -0600

    Catastrophe

--- controllers/index.js
+++ controllers/index.js
@@ -102,7 +102,7 @@ var indexController = {
 		// CHECK IF THE IP USER ALREADY LIKED THIS POST
 		Post.find({_id: req.body._id}, function(err, userData){
 			console.log("userData.likedbyIP.length.BELOW!!!");
-			console.log(userData.likedByIp/length);
+			console.log(userData.likedByIp.length);
 			if(
 				userData.likedByIp === undefined || userData.likedByIp.indexOf(req.body.userIP) === -1
 				){
