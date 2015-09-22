0ac2ac63d0991aec6b01babfa173ba62c0a37585
Jamie Binns <jamie.m.binns@gmail.com>
2015-09-22 09:43:22 -0600

    Freight train of glowing fragments

--- controllers/index.js
+++ controllers/index.js
@@ -104,7 +104,7 @@ var indexController = {
 			console.log("userData.likedbyIP.length.BELOW!!!");
 			console.log(userData.likedByIp.length);
 			if(
-				userData.likedByIp === undefined || userData.likedByIp.indexOf(req.body.userIP) === -1
+				userData.likedByIp == undefined || userData.likedByIp.indexOf(req.body.userIP) === -1
 				){
 					Post.findOneAndUpdate({_id: req.body._id}, {$inc: {likes: 1}}, function(err, userData){
 						// console.log('This is the backend error: ', err)
