4d69c1ff4d3f4f2624ed115b99af2011fff61d6b
marczenn <marcusareulius@gmail.com>
2015-10-23 15:16:00 -0600

    insalled gulp added gulpfile.js and added dependencies

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+// Require Gulp 
+var require = require('gulp'),
+
+	// Require Gulp's plugins. For now we will only be using browser-sync for faster dev updating & testing. 
+	browserSync = ('browser-sync'),
+	// this will reload the browser upon updates to either html or css files
+	reload = browserSync.reload
+
