078cc40818319add36d75039a79207434f73cf64
marczenn <marcusareulius@gmail.com>
2015-10-23 15:31:02 -0600

    added gulp-watch task gulp-jade task gulp-css task and default task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,29 @@ var require = require('gulp'),
 	// this will reload the browser upon updates to either html or css files
 	reload = browserSync.reload
 
+
+// Watch Tasks
+gulp.task('watch', function() {
+    gulp.watch('../client/stylesheets/**/*.css', ['css']);
+    gulp.watch('../client/views/**/*.jade', ['jade']); 
+});
+
+
+// CSS Task
+gulp.task('css', function() {
+	gulp.src('../client/stylesheets/**/*.css')
+})
+
+// HTML Tasks 
+gulp.task('jade ', function() {
+	gulp.src('../client/views/**/*.jade')
+})
+
+
+//browserSync Task 
+
+
+
+
+// Default Task
+gulp.task('default', ['watch', 'jade', 'css']); 
