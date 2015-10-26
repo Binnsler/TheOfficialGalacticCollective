82caac1842c2ec02c8cfca43520649e82d62b0a0
marczenn <marcusareulius@gmail.com>
2015-10-26 11:10:50 -0600

    still attempting to config gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,64 @@ var gulp = require('gulp'),
 	// Require Gulp's plugins. For now we will only be using browser-sync for faster dev updating & testing. 
 	browserSync = require('browser-sync'),
 	// this will reload the browser upon updates to either html or css files
-	reload = browserSync.reload
+	reload = browserSync.reload,
+
+	webserver = require('gulp-webserver');
+
 
 
 // Watch Tasks
 gulp.task('watch', function() {
-    gulp.watch('../client/stylesheets/**/*.css', ['css', browserSync.reload]);
-    gulp.watch('../client/views/**/*.jade', ['jade', browserSync.reload]); 
+    return gulp.watch('../client/stylesheets/**/*.css', ['css']);
+    gulp.watch('../client/views/**/*.jade', ['jade']); 
 });
 
 
 // CSS Task
 gulp.task('css', function() {
-	gulp.src('../client/stylesheets/**/*.css')
+	return gulp.src('../client/stylesheets/**/*.css')
 	.pipe(reload({stream:true}));
 });
 
 // HTML Tasks 
 gulp.task('jade', function() {
-	gulp.src('../client/views/**/*.jade')
+	return gulp.src('../client/views/**/*.jade')
 	.pipe(reload({stream:true}));
 });
 
 
-//browserSync Tasks
-gulp.task('browserSync', function() {
-	browserSync({
-		server: {
-			baseDir: './'
-		}
-	});
-	   // browserSync.init({
-    //     proxy: "172.27.19.56:3000"
-    // });
+// browserSync Tasks
+// gulp.task('browserSync', function() {
+// 	// browserSync({
+// 	// 	server: {
+// 	// 		baseDir: './'
+// 	// 	}
+// 	// });
+// 	browserSync.init(null, {
+//     	proxy: "http://localhost:3000",
+//       	files: ['galacticcollective/**/*'],
+//       	browser: "google chrome",
+//       	port: 3000
+//   });
+// });
+
+gulp.task('webserver', function() {
+ 	gulp.src('galacticcollective')
+   	.pipe(webserver({
+   		port: '3000',
+    	livereload: true,
+    	// directoryListing: true,
+    	open: true,
+    	// fallback: 'home.jade'
+  //   	directoryListing: {
+  //   		enable:true,
+  //   		path: './'
+		// }
+    }));
 });
 
 
 
 
 // Default Tasks
-gulp.task('default', ['jade', 'css', 'browserSync', 'watch']); 
+gulp.task('default', ['jade', 'css', 'watch', 'webserver']); 
