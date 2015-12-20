aabb817b6f502905bb1ee4e2302c48bfae28ae0a
Jamie Binns <jamie.m.binns@gmail.com>
2015-12-20 14:42:22 -0700

    Marc 5th, removed Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -59,13 +59,13 @@ gulp.task('serveprod', function() {
 
 // Watch Tasks: Watch is our build task. initiates all other tasks, then initiates browserSync. 
 gulp.task('watch', ['jade', 'home', 'css', 'image', 'scripts', 'serveprod'], function() {
-	browserSync({
-    	server: {
-    		baseDir: "./dist/"
-		}
-	});
-    gulp.watch('client/css/**/*.css', ['css'], reload);
-    gulp.watch('views/**/*.jade', ['jade'], reload); 
+	// browserSync({
+ //    	server: {
+ //    		baseDir: "./dist/"
+	// 	}
+	// });
+ //    gulp.watch('client/css/**/*.css', ['css'], reload);
+ //    gulp.watch('views/**/*.jade', ['jade'], reload); 
     // gulp.watch(['./**/*.js', '!./node_modules/**/*.js'], ['scripts'], reload); // dont watch node_modules or call stack will be exceeded in terminal. 
 });
 
