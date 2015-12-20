d4cb161b170a7964fb56b6f5fe6b6d998058a29b
Being Human <beinghuman@Beings-MacBook-Pro.local>
2015-12-20 13:40:18 -0700

    Pushing new test of gulp app

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('css', function() {
 // Fire up production server 
 gulp.task('serveprod', function() {
   return connect.server({
-    root: [galacticcollective],
+    root: ['_public'],
     port: process.env.PORT || 5000, // localhost:5000
     livereload: false
   });
