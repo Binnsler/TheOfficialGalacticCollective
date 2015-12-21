27cf0927a3d3dd4f65b44fa4ce420bc9b996139f
Jamie Binns <jamie.m.binns@gmail.com>
2015-12-21 11:27:17 -0700

    Removing /dist from gitignore

---
+++ dist/js/gulpfile.js
@@ -0,0 +1,72 @@
+// Require Gulp 
+var gulp = require('gulp')
+
+// Require Gulp's plugins.
+var connect = require('gulp-connect');
+var jade = require('gulp-jade');
+var image = require('gulp-image');
+var browserSync = require('browser-sync'),
+	// this will live-reload the browser upon updates to either html or css files
+	reload = browserSync.reload
+
+
+// HTML Tasks: takes all Jade files in path, converts them to html and places into /dist folder. live reload is being used.
+gulp.task('jade', function() {
+    return gulp.src(['views/**/*.jade', '!views/**/index.jade'])
+        .pipe(jade()) 
+        .pipe(gulp.dest('./dist/views')) 
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('home', function() {
+    return gulp.src('views/**/index.jade')
+        .pipe(jade()) 
+        .pipe(gulp.dest('./dist')) 
+        .pipe(reload({stream:true}));
+});
+
+
+// Image Tasks: 
+gulp.task('image', function () {
+  gulp.src(['client/images/*.svg', 'client/images/*.png', 'client/images/*.jpg' ])
+    .pipe(image())
+    .pipe(gulp.dest('./dist/images'));
+});
+
+// Scripts Tasks: takes all JS files in path and places into dist/js folder. live reload is being used. 
+gulp.task('scripts', function() {
+	gulp.src('./**/*.js')
+	.pipe(gulp.dest('./dist/js'))
+	.pipe(reload({stream:true}));
+});
+
+
+// CSS Tasks: takes all CSS files in path and places into dist/css folder. live reload is being used.
+gulp.task('css', function() {
+	return gulp.src('client/css/**/*.css')
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(reload({stream:true}));
+});
+
+// Fire up production server 
+gulp.task('serveprod', function() {
+  return connect.server({
+    root: ['_public'],
+    port: process.env.PORT || 8000, // localhost:5000
+    livereload: false
+  });
+});
+
+// Watch Tasks: Watch is our build task. initiates all other tasks, then initiates browserSync. 
+gulp.task('watch', ['jade', 'home', 'css', 'image', 'scripts', 'serveprod'], function() {
+	browserSync({
+    	server: {
+    		baseDir: "./dist/"
+		}
+	});
+    gulp.watch('client/css/**/*.css', ['css'], reload);
+    gulp.watch('views/**/*.jade', ['jade'], reload); 
+    // gulp.watch(['./**/*.js', '!./node_modules/**/*.js'], ['scripts'], reload); // dont watch node_modules or call stack will be exceeded in terminal. 
+});
+
+
